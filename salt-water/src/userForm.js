2074160cd6284715e6d4bf517fd10708c0962f21
eliasgoodale <eligoodale1@gmail.com>
2018-09-27 20:23:58 -0700

    final push for day. form working, validates will do dialogs tomorrow and integrate with update

--- salt-water/src/userForm.js
+++ salt-water/src/userForm.js
@@ -70,7 +70,6 @@ class CreateUserForm extends React.Component {
     isLocationManager: false,
     isOperatorAdmin: false,
     checked: [],
-    open: false,
     sendingForm: false,
     sentForm: false
   };
