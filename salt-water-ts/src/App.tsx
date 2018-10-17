5c33750b2a42c95654a628c12d78108072eff65f
eliasgoodale <eligoodale1@gmail.com>
2018-10-17 16:10:31 -0700

    fixed selection bug

--- salt-water-ts/src/App.tsx
+++ salt-water-ts/src/App.tsx
@@ -19,15 +19,15 @@ import CommandCell from './CommandCell'
 
 
 class CheckboxCell extends Component<any, {}> {
- 
+
   public render() {
     const { dataItem, field, onChange } = this.props
     const value = dataItem[field];
     return (
-      dataItem.inEdit ? 
-      <td>
-  
-         <input type="checkbox" id={field} className="k-checkbox"  checked={value} 
+      dataItem.inEdit ?
+        <td>
+
+          <input type="checkbox" id={field} className="k-checkbox" checked={value}
             onChange={(e: any) => {
               e.dataItem = dataItem
               e.field = field
@@ -35,15 +35,15 @@ class CheckboxCell extends Component<any, {}> {
               onChange(e)
             }}
           />
-          <label className="k-checkbox-label" htmlFor={field}/>
-           
-      </td> :
-      value ?
-            <td>
-            <input type="checkbox" id={field} className="k-checkbox" checked={value}/>
-            <label className="k-checkbox-label"/>
-            </td>
-       : <td/>
+          <label className="k-checkbox-label" htmlFor={field} />
+
+        </td> :
+        value ?
+          <td>
+            <input type="checkbox"  className="k-checkbox" checked={value} />
+            <label className="k-checkbox-label" />
+          </td>
+          : <td />
 
     );
   }
@@ -70,12 +70,12 @@ interface IUser {
   isListAdmin: boolean
   isLocationManager: boolean
   isOperatorAdmin: boolean
-  isUserAdmin: boolean 
+  isUserAdmin: boolean
 }
 
 const blankData: IUser = {
   id: "temp",
-  firstName:"",
+  firstName: "",
   lastName: "",
   password: "",
   username: "",
@@ -84,7 +84,7 @@ const blankData: IUser = {
   isListAdmin: false,
   isLocationManager: false,
   isOperatorAdmin: false,
-  isUserAdmin: false, 
+  isUserAdmin: false,
 }
 
 interface IState {
@@ -109,7 +109,7 @@ const schema: any = Joi.object().keys({
   password: Joi.string().min(6).max(25).required(),
   isActive: Joi.boolean().required(),
   isEntryAdmin: Joi.boolean().required(),
-  isListAdmin: Joi.boolean().required(), 
+  isListAdmin: Joi.boolean().required(),
   isLocationManager: Joi.boolean().required(),
   isOperatorAdmin: Joi.boolean().required(),
   isUserAdmin: Joi.boolean().required(),
@@ -117,7 +117,7 @@ const schema: any = Joi.object().keys({
 
 
 class App extends Component<{}, IState>{
-  
+
   /* Refactor later if constructor wont be needed */
 
   constructor(props: any) {
@@ -131,7 +131,7 @@ class App extends Component<{}, IState>{
       showInactive: false,
       removeAlertOpen: false,
       lockEdit: false,
-      sort: [{field: 'username', dir: 'asc'}],
+      sort: [{ field: 'username', dir: 'asc' }],
       newPassword: "",
       confirmPassword: "",
     }
@@ -148,16 +148,16 @@ class App extends Component<{}, IState>{
 
   public render() {
     const { tableData, editID, showInactive, lockEdit } = this.state
-    const filterData = showInactive ? 
-    tableData.filter( (u: any) => !u.isActive ) :
-    tableData.filter( (u: any) => u.isActive )
-    
+    const filterData = showInactive ?
+      tableData.filter((u: any) => !u.isActive) :
+      tableData.filter((u: any) => u.isActive)
+
     return (
       <Fragment>
         <Grid
-          
-          style={{ height: '500px'}}
-          data={ orderBy( filterData.map((user: IUser) =>
+
+          style={{ height: '500px' }}
+          data={orderBy(filterData.map((user: IUser) =>
             Object.assign({
               inEdit: user.id === editID
             }, user)), this.state.sort)
@@ -178,17 +178,17 @@ class App extends Component<{}, IState>{
           <GridToolbar>
             <div>
 
-              <button 
-                title="Add new" 
-                className="k-button k-primary" 
+              <button
+                title="Add new"
+                className="k-button k-primary"
                 onClick={this.enterInsert}
                 disabled={editID !== null}
               >
-              Add User
+                Add User
               </button>
 
-              <button 
-                className="k-button k-primary" 
+              <button
+                className="k-button k-primary"
                 disabled={editID === null || !this.validSubmission()}
                 onClick={this.save}
               >
@@ -207,7 +207,7 @@ class App extends Component<{}, IState>{
                 className="k-button k-primary"
                 disabled={editID === null}
                 onClick={this.cancel}
-              > 
+              >
                 Cancel
               </button>
             </div>
@@ -215,16 +215,16 @@ class App extends Component<{}, IState>{
           <Column field="username" title="Username" width="200px" />
           <Column field="firstName" title="First Name" width="200px" />
           <Column field="lastName" title="Last Name" width="200px" />
-          <Column field="isEntryAdmin" title="Entry Admin" editor="boolean" 
+          <Column field="isEntryAdmin" title="Entry Admin" editor="boolean"
             cell={(props) => <CheckboxCell {...props} />} />
-          <Column field="isListAdmin" title="List Admin" editor="boolean" 
-            cell={(props) => <CheckboxCell {...props} />}/>
-          <Column field="isLocationManager" title="Location Manager" editor="boolean" 
-            cell={(props) => <CheckboxCell {...props} />}/>
-          <Column field="isOperatorAdmin" title="Operator Admin" editor="boolean" 
-            cell={(props) => <CheckboxCell {...props} />}/>
-          <Column field="isUserAdmin" title="User Admin" editor="boolean" 
-            cell={(props) => <CheckboxCell {...props} />}/>
+          <Column field="isListAdmin" title="List Admin" editor="boolean"
+            cell={(props) => <CheckboxCell {...props} />} />
+          <Column field="isLocationManager" title="Location Manager" editor="boolean"
+            cell={(props) => <CheckboxCell {...props} />} />
+          <Column field="isOperatorAdmin" title="Operator Admin" editor="boolean"
+            cell={(props) => <CheckboxCell {...props} />} />
+          <Column field="isUserAdmin" title="User Admin" editor="boolean"
+            cell={(props) => <CheckboxCell {...props} />} />
           <Column
             cell={CommandCell(this.togglePasswordModal, this.reactivateUser)}
             width="150px"
@@ -247,15 +247,15 @@ class App extends Component<{}, IState>{
 
                 />
               </label>
-                <br/>
-                <label>
-                  Confirm Password <br />
-                  <Input
-                    type="text"
-                    name="confirmPassword"
-                    value={this.state.confirmPassword}
-                    onChange={this.handlePasswordInput}
-                  />
+              <br />
+              <label>
+                Confirm Password <br />
+                <Input
+                  type="text"
+                  name="confirmPassword"
+                  value={this.state.confirmPassword}
+                  onChange={this.handlePasswordInput}
+                />
               </label>
 
             </div>
@@ -277,15 +277,15 @@ class App extends Component<{}, IState>{
             </button>
           </DialogActionsBar>
 
-          </Dialog>}
-          
-          {this.state.removeAlertOpen && <Dialog
-            title="Remove User?"
-            onClose={this.toggleRemoveAlert}
-            >
-            <label>
-              Are you sure you want to delete this user? <br/>
-            </label>
+        </Dialog>}
+
+        {this.state.removeAlertOpen && <Dialog
+          title="Remove User?"
+          onClose={this.toggleRemoveAlert}
+        >
+          <label>
+            Are you sure you want to delete this user? <br />
+          </label>
           <DialogActionsBar>
             <button
               className="k-button"
@@ -300,14 +300,14 @@ class App extends Component<{}, IState>{
               Confirm
             </button>
           </DialogActionsBar>
-            </Dialog>
-          }
-      <button
-        className="k-button k-primary "
-        onClick={this.toggleInactiveUsers}
-      >
-      { showInactive ? "Show Active" : "Show Inactive"}
-      </button>
+        </Dialog>
+        }
+        <button
+          className="k-button k-primary "
+          onClick={this.toggleInactiveUsers}
+        >
+          {showInactive ? "Show Active" : "Show Inactive"}
+        </button>
       </Fragment>
     );
   }
@@ -317,15 +317,15 @@ class App extends Component<{}, IState>{
     const value: string = e.target.value
     if (name === 'newPassword') {
       this.setState({ newPassword: value })
-    } else if(name === 'confirmPassword') {
-        this.setState({ confirmPassword: value })
+    } else if (name === 'confirmPassword') {
+      this.setState({ confirmPassword: value })
     }
   }
 
   private passwordValid = (): boolean => {
     const { newPassword, confirmPassword } = this.state
     const passwordSchema: any = Joi.string().min(6).max(25)
-    if(newPassword === confirmPassword) {
+    if (newPassword === confirmPassword) {
       const result: any = Joi.validate(confirmPassword, passwordSchema)
       return !result.error ? true : false
     }
@@ -333,8 +333,8 @@ class App extends Component<{}, IState>{
   }
   private savePassword = (): void => {
     const newTableData: any = this.state.tableData.slice()
-    const index: number = newTableData.findIndex( (u: any) => u.id === this.state.editID)
-    newTableData[index] = {...newTableData[index], password: this.state.confirmPassword}
+    const index: number = newTableData.findIndex((u: any) => u.id === this.state.editID)
+    newTableData[index] = { ...newTableData[index], password: this.state.confirmPassword }
     this.togglePasswordModal()
     this.setState({
       tableData: newTableData
@@ -345,7 +345,7 @@ class App extends Component<{}, IState>{
     console.log("called")
     const editData: any = this.state.tableData.slice()
     const index: number = editData.findIndex((u: any) => u.id === e.dataItem.id)
-           
+
     editData[index] = { ...editData[index], [e.field]: e.value }
     this.setState({
       tableData: editData,
@@ -355,7 +355,7 @@ class App extends Component<{}, IState>{
 
   private reactivateUser = (): void => {
     const newTableData = this.state.tableData.slice()
-    const index: number = newTableData.findIndex( (u: any) => u.id === this.state.editID)
+    const index: number = newTableData.findIndex((u: any) => u.id === this.state.editID)
     newTableData[index].isActive = true
     this.setState({
       editID: null,
@@ -365,7 +365,7 @@ class App extends Component<{}, IState>{
 
   // private toggleCheckbox = (field: string): void => {
   //   this.setState({
-      
+
   //   })
   // } 
 
@@ -373,7 +373,7 @@ class App extends Component<{}, IState>{
     this.setState({
       newPassword: "",
       confirmPassword: ""
-    }) 
+    })
   }
 
   private togglePasswordModal = (): void => {
@@ -396,19 +396,19 @@ class App extends Component<{}, IState>{
   }
 
   /* Not sure if Promise<IUser[]> return type will break my code in some cases */
-  private getUsers = async(): Promise<IUser[]> => {
+  private getUsers = async (): Promise<IUser[]> => {
     const request: any = await fetch(API_URL)
     const data: any = await request.json()
     return data
   }
 
 
-  private patchUser = async(updateUser: IUser, originalUser: IUser): Promise<void> => {
-    
+  private patchUser = async (updateUser: IUser, originalUser: IUser): Promise<void> => {
+
     const diff: any = jsonpatch.compare(updateUser, originalUser)
     console.log(`PATCH payload:\n`)
     console.table(diff)
-    
+
     // const settings: any = {
     //   method: 'PATCH',
     //   headers: {
@@ -418,14 +418,14 @@ class App extends Component<{}, IState>{
     // }
     // const response: any = await fetch(`${API_URL}/${originalUser.id}`)
     // const json: any = await response.json()
-    
+
   }
 
-  private postUser = async(newUser: IUser): Promise<void> => {
-   console.log(`POST payload:\n`)
-   delete newUser.id
-   console.table(newUser)
-   
+  private postUser = async (newUser: IUser): Promise<void> => {
+    console.log(`POST payload:\n`)
+    delete newUser.id
+    console.table(newUser)
+
     // const settings: any = {
     //   methods: 'POST',
     //   headers: {
@@ -436,7 +436,7 @@ class App extends Component<{}, IState>{
     // }
     // const response: any = await fetch(API_URL, settings)
     // const json: any = await response.json()
-    
+
   }
 
 
@@ -445,11 +445,11 @@ class App extends Component<{}, IState>{
     let changed: boolean = true
     if (editID) {
       const selectedUser: any = tableData.find((u: any) => u.id === editID)
-      if(editID !== "temp") {
+      if (editID !== "temp") {
         const originalUser: IUser = userData.find((u: IUser) => u.id === editID)
         changed = jsonpatch.compare(originalUser, selectedUser).length === 0 ? false : true
       }
-      const result: any = Joi.validate({...selectedUser}, schema)
+      const result: any = Joi.validate({ ...selectedUser }, schema)
       return result.error || !changed ? false : true
     }
     return false
@@ -470,13 +470,13 @@ class App extends Component<{}, IState>{
       showInactive: false,
     })
   }
-  
+
   private rowClick = (e: any): void => {
     if (this.state.lockEdit) {
-      return 
+      return
     }
     const { tableData } = this.state
-    const index: number = tableData.findIndex( (u: any) => u.id === e.dataItem.id)
+    const index: number = tableData.findIndex((u: any) => u.id === e.dataItem.id)
     const selectedUser: any = tableData[index]
     if (this.state.editID !== null) {
       this.cancel()
@@ -491,9 +491,9 @@ class App extends Component<{}, IState>{
     if (this.state.passwordModalOpen) {
       this.togglePasswordModal()
     }
-    
+
     const { editID } = this.state
-    if (editID === null){
+    if (editID === null) {
       return
     }
     const newTableData: any = this.state.tableData.slice()
@@ -512,20 +512,20 @@ class App extends Component<{}, IState>{
   }
 
   private clone = (data: any): any => {
-    return Object.assign({}, data)  
-  } 
+    return Object.assign({}, data)
+  }
 
 
 
 
 
   private save = (): void => {
-    const {editID} = this.state
+    const { editID } = this.state
     const newTableData = this.state.tableData.slice()
     const newUserData = this.state.userData.slice()
     const userIndex = newUserData.findIndex((u: any) => u.id === editID)
     const tableIndex: number = newTableData.findIndex((u: any) => u.id === editID)
-  
+
     if (editID === "temp") {
       this.postUser(newTableData[tableIndex])
       newTableData[tableIndex].id = generateID() // Watch for post return json
@@ -543,7 +543,7 @@ class App extends Component<{}, IState>{
 
   }
 
-  
+
 
   private remove = (): void => {
     if (this.state.removeAlertOpen) {
@@ -551,20 +551,20 @@ class App extends Component<{}, IState>{
     }
     const newTableData: any = this.state.tableData.slice()
     const newUserData: any = this.state.userData.slice()
-    const tableIndex: number = newTableData.findIndex( (u: any) => u.id === this.state.editID )
-    const userIndex: number = newUserData.findIndex( (u: any) => u.id === this.state.editID)
+    const tableIndex: number = newTableData.findIndex((u: any) => u.id === this.state.editID)
+    const userIndex: number = newUserData.findIndex((u: any) => u.id === this.state.editID)
     newTableData[tableIndex].isActive = false
     this.patchUser(newTableData[tableIndex], newUserData[userIndex])
     newUserData[userIndex].isActive = false
     this.setState({
       editID: null,
-      tableData: newTableData, 
+      tableData: newTableData,
       userData: newUserData
     })
   }
 }
 
- 
+
 
 
 export default App;
